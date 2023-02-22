7a03ee4bf8ef890f8821e3c38f88546cd5b960c7
Evans Rutto <robbyevans001@gmail.com>
2023-02-22 17:25:57 +0300

    create works component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import Expertise from './components/Experties/Expertise'
+import Works from './components/Works/Works'
 import css from "./styles/app.module.scss"
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <Hero/>
       <Expertise/>
+      <Works/>
     </div> 
   )
 }
--- src/components/Experties/Expertise.jsx
+++ src/components/Experties/Expertise.jsx
@@ -1,71 +1,63 @@
 import React from 'react'
 import { projectExperience, WhatDoIHelp } from '../../utils/data'
-import { fadeIn, staggerContainer ,textVariant} from '../../utils/motion'
+import css from './Expertise.module.scss'
 import {motion} from 'framer-motion'
-import css from "./Expertise.module.scss"
-
-function Expertise() {
+import {fadeIn, staggerContainer, textVariant} from '../../utils/motion.js'
+const Experties = () => {
   return (
-    <section
-    
-    className={`${css.wrapper}`}>
+    <section 
+    className={css.wrapper}>
+        <a className="anchor" id="experties"></a>
+        <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{once: false, amount: 0.25}}
+        className={`paddings yPaddings innerWidth flexCenter ${css.container}`}>
 
-      <motion.div
-      variant={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{once: false, amount:0.25}}
-       className={`paddings yPaddings flexCenter innerWidth ${css.container}`}>
 
-      {/* left side */}
-        <div className={css.leftSide}>
-
-          {/* experience cards */}
-          {
-            projectExperience.map((exp,i)=>{
-              return <motion.div
-              
-              variant={fadeIn("right","tween",(i+1)*0.2, 1)}
-               className={css.exp} key={i}>
-                <div className="flexCenter" style={{background:exp.bg}}>
-                  <exp.icon size={25} color="white"/>
-                </div>
-
-                <div>
-                  <span>{exp.name}</span>
-                  <span className="secondaryText"> {exp.projects}</span>
-                  </div>
-
-              </motion.div>
-            })
-          }
-        </div>
-
-        {/* right side */} 
-        <div className={css.rightSide}>
-          <span className="primaryText">What do i help? </span>
-            {
-              WhatDoIHelp.map((paragraph,i)=>{
-                return <span className="secondaryText" key={i}>{paragraph}</span>
-              })
-            }
-
-            <div className={`flexCenter ${css.stats}`}>
-              <div className={`flexCenter ${css.stat}`}>
-                <span className='primaryText'>285+</span>
-                <span className='secondaryText'>projects completed</span>
-              </div>
-              <div className={`flexCenter ${css.stat}`}>
-                <span className='primaryText'>190+</span>
-                <span className='secondaryText'>Happy Clients</span>
-              </div>
+            {/* left side */}
+            <div className={css.leftSide}>
+                {
+                    projectExperience.map((exp, i)=> {
+                        return <motion.div variants = {fadeIn("right", "tween", (i+1)*0.2, 1)} className={css.exp} key={i}>
+                            <div style={{background: exp.bg}} className="flexCenter">
+                                <exp.icon size={25} color="white"/>
+                            </div>
+                            <div>
+                                <span>{exp.name}</span>
+                                <span className='secondaryText'>{exp.projects} Projects</span>
+                            </div>
+                            
+                        </motion.div>
+                    })
+                }
             </div>
-         
-        </div>
 
-      </motion.div>
+
+            {/* right */}
+            <motion.div
+            variants={textVariant(0.5)}
+            className={css.rightSide}>
+
+                <span className='primaryText'>What do I help? </span>
+                {WhatDoIHelp.map((paragraph, i)=> <span className='secondaryText' key={i}>{paragraph}</span>)}
+
+
+                <div className={`flexCenter ${css.stats}`}>
+                    <div className={`flexCenter ${css.stat}`}>
+                        <span className='primaryText'>285+</span>
+                        <span className='secondaryText'>Project Completed</span>
+                    </div>
+                    <div className={`flexCenter ${css.stat}`}>
+                        <span className='primaryText'>190+</span>
+                        <span className='secondaryText'>Happy Clients</span>
+                    </div>
+                </div>
+            </motion.div>
+        </motion.div>
     </section>
   )
 }
 
-export default Expertise
+export default Experties
