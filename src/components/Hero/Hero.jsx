12b58e4f91ad7c501daee1369138f9e0b8129d72
Evans Rutto <robbyevans001@gmail.com>
2023-02-22 12:47:22 +0300

    made hero responsive

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,10 +8,23 @@ function Hero() {
       <div className={`innerWidth ${css.container}`}>
 
         {/* upper element */}
-          <div className={`${css.upperElement}`}></div>
-              
-              <span>Hey There,<br/>I`m Kiprop.</span>
-              <span>I create beautiful things <br/>on the web</span>
+          <div className={`${css.upperElement}`}>
+
+              <span
+              className='primaryText'>Hey There,<br/>I`m Kiprop.</span>
+              <span className='secondaryText'>I create beautiful things <br/>on the web</span>
+
+          </div>
+              {/* person image */}
+              <div className={css.person}>
+                <img src='./person.png' alt=''/>
+              </div>
+
+              {/* email */}
+              <a href="mailto:ruttoevans001@gmail.com" className={css.email}>
+                ruttoevans001@gmail
+              </a>
+
 
               {/* lower element */}
           <div className={`${css.lowerElement}`}>
@@ -24,7 +37,12 @@ function Hero() {
                 <div>Experience</div>
               </div>
             </div>
-            <div className={css.certificate}></div>
+            <div className={css.certificate}>
+              <img src="./certificate.png" alt=""
+              />
+              <span>CERTIFIED PROFATIONAL</span>
+              <span>SOFTWARE DEVELOPER</span>
+            </div>
           </div>
 
       </div>
