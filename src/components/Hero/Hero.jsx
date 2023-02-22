307a2f3d97f117207dff75fe37715f46887a3908
Evans Rutto <robbyevans001@gmail.com>
2023-02-22 13:48:15 +0300

    completed Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,51 +1,71 @@
 import React from 'react'
 import css from './Hero.module.scss'
+import {motion} from "framer-motion"
+import { staggerContainer,fadeIn,slideIn } from '../../utils/motion'
 
 function Hero() {
   return (
     <section
     className= {`paddings ${css.wrapper}`}>
-      <div className={`innerWidth ${css.container}`}>
+      <motion.div className={`innerWidth ${css.container}`}
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{once:false,amount:0.25}}>
 
         {/* upper element */}
           <div className={`${css.upperElement}`}>
 
-              <span
-              className='primaryText'>Hey There,<br/>I`m Kiprop.</span>
-              <span className='secondaryText'>I create beautiful things <br/>on the web</span>
+              <motion.span
+              variants={fadeIn("right","tween",0.2,1)}
+              className='primaryText'>Hey There,<br/>I`m Kiprop.
+              </motion.span>
+              <motion.span
+               variants={fadeIn("left","tween",0.2,1)}
+               className='secondaryText'>I create beautiful things <br/>on the web
+               </motion.span>
 
           </div>
+
               {/* person image */}
               <div className={css.person}>
-                <img src='./person.png' alt=''/>
+                <motion.img
+                variants={slideIn('up','tween',0.5,1.3)}
+                 src='./person.png' alt=''/>
               </div>
 
               {/* email */}
-              <a href="mailto:ruttoevans001@gmail.com" className={css.email}>
+              <motion.a
+              variants={fadeIn("right","tween",0.2,1)}
+               href="mailto:ruttoevans001@gmail.com" className={css.email}>
                 ruttoevans001@gmail
-              </a>
+              </motion.a>
 
 
               {/* lower element */}
           <div className={`${css.lowerElement}`}>
-            <div className={css.experience}>
+            <motion.div className={css.experience}
+            variants={fadeIn("right","tween",0.2,1)}>
 
-              <div className="primaryText">10</div>
+              <div className="primaryText"
+              >3+</div>
               <div className='secondaryText'>
 
                 <div>Years</div>
                 <div>Experience</div>
               </div>
-            </div>
-            <div className={css.certificate}>
+            </motion.div>
+            <motion.div
+            variants={fadeIn("left","tween",0.2,1)}
+             className={css.certificate}>
               <img src="./certificate.png" alt=""
               />
-              <span>CERTIFIED PROFATIONAL</span>
+              <span>CERTIFIED</span>
               <span>SOFTWARE DEVELOPER</span>
-            </div>
+            </motion.div>
           </div>
 
-      </div>
+      </motion.div>
     </section>
   )
 }
